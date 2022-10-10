a31469916d6f7b3285add1fc2a56f3d0e8f666c9
Lxmon <904455569@qq.com>
2022-10-10 19:10:00 +0800

    loops finish

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,70 @@
 fn main() {
     println!("Hello, world!");
+
+    // loop循环
+    // loop {
+    //     println!("again!");
+    // }
+
+    // 循环标签
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up; // 退出外层循环
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    // 循环返回
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // while条件循环
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // for循环
+    let a = [10, 20, 30, 40, 50];
+    // let mut index = 0;
+    // while index < 5 {
+    //     println!("the value is: {}", a[index]);
+    //
+    //     index += 1;
+    // }
+    for element in a {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
